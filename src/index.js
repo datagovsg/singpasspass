cfd386d5ecfae935700ee15d074cff548b9ad3d3
Nikhil <nikhil@data.gov.sg>
2019-01-05 22:51:27 +0800

    added an account model and barebone views

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // see previous example for the things that are not commented
 
 const assert = require('assert');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
 const Provider = require('oidc-provider');
 
 // since dyno metadata is no longer available, we infer the app name from heroku remote we set
@@ -29,13 +32,39 @@ assert(
 // require the redis adapter factory/class
 const RedisAdapter = require('./redis_adapter');
 
+// simple account model for this application, user list is defined like so
+const Account = require('./account');
+
 const oidc = new Provider(
   `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`,
   {
+    // oidc-provider only looks up the accounts by their ID when it has to read the claims,
+    // passing it our Account model method is sufficient, it should return a Promise that resolves
+    // with an object with accountId property and a claims method.
+    findById: Account.findById,
+
+    // let's tell oidc-provider we also support the email scope, which will contain email and
+    // email_verified claims
+    claims: {
+      // scope: [claims] format
+      openid: ['sub'],
+      email: ['email', 'email_verified'],
+    },
+
+    // let's tell oidc-provider where our own interactions will be
+    // setting a nested route is just good practice so that users
+    // don't run into weird issues with multiple interactions open
+    // at a time.
+    interactionUrl(ctx) {
+      return `/interaction/${ctx.oidc.uuid}`;
+    },
     formats: {
       AccessToken: 'jwt',
     },
     features: {
+      // disable the packaged interactions
+      devInteractions: false,
+
       claimsParameter: true,
       discovery: true,
       encryption: true,
@@ -57,7 +86,7 @@ oidc
       // reconfigured the foo client for the purpose of showing the adapter working
       {
         client_id: 'foo',
-        redirect_uris: ['https://peaceful-yonath-ac1071.netlify.com'],
+        redirect_uris: ['https://example.com'],
         response_types: ['id_token token'],
         grant_types: ['implicit'],
         token_endpoint_auth_method: 'none',
@@ -69,5 +98,63 @@ oidc
   .then(() => {
     oidc.proxy = true;
     oidc.keys = process.env.SECURE_KEY.split(',');
-    oidc.listen(process.env.PORT);
+  })
+  .then(() => {
+    // let's work with express here, below is just the interaction definition
+    const expressApp = express();
+    expressApp.set('trust proxy', true);
+    expressApp.set('view engine', 'ejs');
+    expressApp.set('views', path.resolve(__dirname, 'views'));
+
+    const parse = bodyParser.urlencoded({ extended: false });
+
+    expressApp.get('/interaction/:grant', async (req, res) => {
+      oidc.interactionDetails(req).then((details) => {
+        console.log(
+          'see what else is available to you for interaction views',
+          details,
+        );
+
+        const view = (() => {
+          switch (details.interaction.reason) {
+            case 'consent_prompt':
+            case 'client_not_authorized':
+              return 'interaction';
+            default:
+              return 'login';
+          }
+        })();
+
+        res.render(view, { details });
+      });
+    });
+
+    expressApp.post('/interaction/:grant/confirm', parse, (req, res) => {
+      oidc.interactionFinished(req, res, {
+        consent: {
+          // TODO: add offline_access checkbox to confirm too
+        },
+      });
+    });
+
+    expressApp.post('/interaction/:grant/login', parse, (req, res, next) => {
+      Account.authenticate(req.body.email, req.body.password)
+        .then(account => oidc.interactionFinished(req, res, {
+            login: {
+              account: account.accountId,
+              remember: !!req.body.remember,
+              ts: Math.floor(Date.now() / 1000),
+            },
+            consent: {
+              rejectedScopes: req.body.remember ? [] : ['offline_access'],
+            },
+          }),)
+        .catch(next);
+    });
+
+    // leave the rest of the requests to be handled by oidc-provider, there's a catch all 404 there
+    expressApp.use(oidc.callback);
+
+    // express listen
+    expressApp.listen(process.env.PORT);
   });
